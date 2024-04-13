2f9f6ece1f425dcc7cbebef5baf7a827adb76706
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2024-04-14 00:32:07 +0530

    feat: solid + zerops ready

---
+++ src/components/Register.tsx
@@ -0,0 +1,11 @@
+export function Register() {
+  return (
+    <div class="fixed hidden md:block bottom-20 right-5 z-50 items-center justify-center">
+      <a href="https://app.zerops.io/registeration" target="_blank">
+        <div class=" font-bold rounded-md bg-[#EEF4FA] p-3 px-5 py-3 font-mono text-xs hover:bg-[#e1eaf1] text-[#568FCB] duration-100 ">
+          Register and Get up to $100 free Credit 🎉{" "}
+        </div>
+      </a>
+    </div>
+  );
+}
---
+++ src/components/Steps.tsx
@@ -0,0 +1,43 @@
+import { Code } from "./Code";
+import dashboard from  "../public/dashboard.webp";
+
+
+export function Steps() {
+    const ZeropsYaml = `project:
+  name: zerops-solid
+
+services:
+  - hostname: solidstatic
+    type: nginx@1.22
+    nginxConfig: |-
+      server {
+          listen 80 default_server;
+          listen [::]:80 default_server;
+
+          server_name _;
+          root /var/www/out;
+
+          location / {
+              try_files $uri $uri/ /index.html;
+          }
+
+          access_log syslog:server=unix:/dev/log,facility=local1 default_short;
+          error_log syslog:server=unix:/dev/log,facility=local1;
+      }
+    buildFromGit: https://github.com/fxck/zerops-solid-static
+    enableSubdomainAccess: true
+    minContainers: 1`.trim();
+    return (
+        <div class="my-8">
+        <h1 class="text-3xl font-semibold">Step 1: Go To Zerops Dashboard</h1>
+          <img src={dashboard} class="rounded-lg my-6" alt="dashboard image" />
+            <p class="text-[#568FCB] text-xl font-medium">Click on the 'Import Project' button on the sidebar. (Not a user? Register now)</p>
+            <div class="mt-16 pb-4">
+              <h1 class="text-3xl font-semibold">Step 2: Paste the project Yaml</h1>
+              <p class="text-[#568FCB] text-xl font-medium mt-4">Copy the YAML code mentioned below and paste it to import the project. Alternatively, you can clone zerops-solid-static to your GitHub profile and then replace the repository URL in the buildFromGit parameter.</p>
+              <p class="text-[#568FCB] text-xl font-medium mt-3">For more information, explore our docs and if you still find yourself stuck in the process, join our Discord community.</p>
+            </div>
+            <Code code={ZeropsYaml} />
+        </div>
+    );
+}
